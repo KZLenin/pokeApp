58eb887c125e32c397c156066e5f8db294c24866
KZLenin <leninmateo4@gmail.com>
2025-10-28 16:40:23 -0500

    poke

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkk1YfsGN-JVqqAqh_F4d1Mp92eCD1O4Q",
+  authDomain: "pokeapi-eb858.firebaseapp.com",
+  projectId: "pokeapi-eb858",
+  storageBucket: "pokeapi-eb858.firebasestorage.app",
+  messagingSenderId: "652900675417",
+  appId: "1:652900675417:web:f87a58c3c53d370627e648"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
